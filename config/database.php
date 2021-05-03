b1bfbee0d07024435ef7c99a046dee6be70d339f
ocha-rowca <56023196+ocha-rowca@users.noreply.github.com>
2021-05-03 23:09:14 +0000

    change dbname

--- config/database.php
+++ config/database.php
@@ -201,7 +201,7 @@ else {
                 'url' => env('DATABASE_URL'),
                 'host' => env('DB_HOST', '127.0.0.1'),
                 'port' => env('DB_PORT', '3306'),
-                'database' => 'aa13n3m3werkt25',
+                'database' => 'ebdb',
                 'username' => env('DB_USERNAME', 'root'),
                 'password' => env('DB_PASSWORD', ''),
                 'unix_socket' => env('DB_SOCKET', ''),
