9acdf00ba748fb3e97c1006e31db045e1fc98009
ocha-rowca <56023196+ocha-rowca@users.noreply.github.com>
2021-07-27 21:57:21 +0000

    Update import.blade.php

--- resources/views/localdata/import.blade.php
+++ resources/views/localdata/import.blade.php
@@ -24,7 +24,7 @@
                     </div>
                 @endif
 
-                <p class="h1 mb-3">Importation des données (1)</p>
+                <p class="h1 mb-3">Importation des données (2)</p>
                 <form action="/database/guide_import" method="POST" enctype="multipart/form-data" class=" g-3 needs-validation" novalidate>
                     @csrf
                     <div class="mb-3">
